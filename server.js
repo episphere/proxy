dc9ebe61d5f9f75303ce93b8f4ce41cfd1f1520e
Jonas Almeida <jalmeida@mathbiol.org>
2022-10-11 16:33:56 -0400

    proxy proxying :-)

--- server.js
+++ server.js
@@ -6,26 +6,17 @@ http
   .createServer(function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    url = 'https://data.cdc.gov/resource/muzy-jte6.json?$limit=3'
-    console.log('server started')
-    axios.get(url).then(x => {
-      console.log('data retrieved')
-      res.end('hello proxy')
-    })
+    //url = 'https://data.cdc.gov/resource/muzy-jte6.json?$limit=3'
+    url = req.url.slice(2)
+    console.log(`server ready ${Date()}`)
+    if(req.url!='/favicon.ico'){
+      axios.get(url).then(x => {
+        console.log(`data retrieved from ${url}`)
+        //res.end(`:-)`)
+        res.end(JSON.stringify(x.data))
+      })
+    }else{
+      res.end()
+    }
+      
   }).listen(3000); //the server object listens on port 8080
-/*
-                
-    //debugger
-    //res.write(`Hello World from Jonas! at ${Date()}`); //write a response to the client
-    axios.get(url).then(x => {
-      //console.log(`---${Date()}---\n`,x.data)
-      //console.log(req)
-      //res.end(JSON.stringify(x.data))
-      //res.end('hello')
-      //console.log(req)
-      res.end(`Hello proxy at ${Date()}`)
-    })
-    //res.end(); //end the response
-  })
-  .listen(3000); //the server object listens on port 8080
-*/
