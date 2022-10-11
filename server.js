86891151afc9b9c5791aee4eea210f2d23df1d52
Jonas Almeida <jalmeida@mathbiol.org>
2022-10-11 19:20:37 +0000

    hello test

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ http
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
     url = 'https://data.cdc.gov/resource/muzy-jte6.json?$limit=3'
+    axios.get(url).then(x => {
+      res.end('hello proxy')
+    })
+  }).listen(3000); //the server object listens on port 8080
+/*
+                
     //debugger
     //res.write(`Hello World from Jonas! at ${Date()}`); //write a response to the client
     axios.get(url).then(x => {
@@ -14,9 +20,10 @@ http
       //console.log(req)
       //res.end(JSON.stringify(x.data))
       //res.end('hello')
-      console.log(req)
+      //console.log(req)
       res.end(`Hello proxy at ${Date()}`)
     })
     //res.end(); //end the response
   })
   .listen(3000); //the server object listens on port 8080
+*/
