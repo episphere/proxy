2d82490c579995bc04e1c8ddac783ac069e8d6fb
Jonas Almeida <jalmeida@mathbiol.org>
2022-11-11 08:57:20 -0500

    cloudFlare hosted proxy

--- cloudflare.js
+++ cloudflare.js
@@ -1,19 +1,43 @@
-var url2
+// https://proxy.jalmeida.workers.dev/?https://cog.sanger.ac.uk/cosmic-signatures-production/documents/COSMIC_v3.3.1_SBS_GRCh37.txt
+// https://covid19serohub.nih.gov/api/v1/seroprevalence_totals
+
+var urlArray=[ // valid url patterns
+    /^https:\/\/cog\.sanger\.ac\.uk\/cosmic-signatures-production\/documents\/COSMIC_v3\.3\.1_SBS_GRCh3[78]\.txt$/,
+    /^https:\/\/www\.pgscatalog\.org\/rest\//,
+    /^https:\/\/covid19serohub\.nih\.gov\/api\/v1\/seroprevalence/,
+    /^https:\/\/api.ncbi.nlm.nih.gov\/variation\//
+]
+
+function urlMatch (url){
+    url=decodeURIComponent(url)
+    return urlArray.filter(u=>url.match(u)).length>0
+}
+
 var src_default = {
   async fetch(request, env, ctx) {
-    console.log(request,url2,'search:',url2.search)
-    let url='https://data.cdc.gov/resource/muzy-jte6.json?$limit=2'
-    url2=new URL(request.url)
-    
-    let res = await (await fetch(url)).json()
+    let search = decodeURIComponent((new URL(request.url)).search.slice(1))
+    console.log('search',search,urlMatch(search))
+    let res={ // default response
+        proxyHost: "floudflare",
+        msg: "url not provided or not supported"
+    }
+    if(urlMatch(search)){
+        let url=search
+        if(url.match(/\.txt$/)!=null|url.match(/\.csv$/)!=null|url.match(/\.tsv$/)!=null){
+            res = await (await fetch(url)).text()
+        }else{
+            res = await (await fetch(url)).json()
+        }
+    }
     return new Response(JSON.stringify(res),{
       headers: {
         //'content-type': 'text/html;charset=UTF-8',
         'Access-Control-Allow-Origin': '*'
       },
-  });
+    });
   }
 };
+
 export {
   src_default as default
 };
